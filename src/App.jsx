c3c76c8e64df4741dde3b0eee4dd9a03b18b89b7
Essohanam TAMBANA <114746721+GhostEsso@users.noreply.github.com>
2024-02-21 12:07:15 +0000

    Revert "NavBar Created"

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import React from 'react';
-import Navbar from './components/Navbar.jsx'; // Importe le composant Navbar correctement
-import './components/navbar.css'; // Assure-toi d'importer le fichier de styles correctement
 
 const App = () => {
   return (
-    <>
-      <Navbar />
-      {/* Ajoute ici les autres composants ou contenus de ton application */}
-    </>
+    <div>
+      
+    </div>
   );
 };
 
