1796f8528846d7334b19a532ff402a0215e796f5
GhostEsso <lilipitaham@gmail.com>
2024-02-21 13:57:07 +0000

    dashbaord added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './components/Navbar.jsx'; // Importe le composant Navbar correctement
 import './components/navbar.css'; // Assure-toi d'importer le fichier de styles correctement
+import Dashboard from './components/Dashboard.jsx';
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <Dashboard />
       {/* Ajoute ici les autres composants ou contenus de ton application */}
     </>
   );
