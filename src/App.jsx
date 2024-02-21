5025d5d9ca301caafb5736bdba9e84f24ee402dc
GhostEsso <lilipitaham@gmail.com>
2024-02-21 19:28:29 +0000

    design updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Navbar from './components/Navbar.jsx'; // Importe le composant Navbar correctement
 import './components/navbar.css'; // Assure-toi d'importer le fichier de styles correctement
 import Dashboard from './components/Dashboard.jsx';
+import Weather from './components/Weather.jsx';
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Dashboard />
+      <Weather />
+
       {/* Ajoute ici les autres composants ou contenus de ton application */}
     </>
   );
