dca52fdfec1d76ad8456c4eae3855e34860a2f44
GhostEsso <lilipitaham@gmail.com>
2024-02-24 17:39:49 +0000

    Tilte section added

--- src/components/Second/SecondPage.jsx
+++ src/components/Second/SecondPage.jsx
@@ -1,11 +1,14 @@
 // SevenDayForecast.jsx
 import Navbar from "./Navbar";
+import Title from "./Title";
+
 import "./SecondPage.css";
 
 const SecondPage = () => {
   return (
     <div className="secondPage">
       <Navbar />
+      <Title />
     </div>
   );
 };
---
+++ src/components/Second/Title.jsx
@@ -0,0 +1,16 @@
+import "./Title.css";
+
+const Title = () => {
+  return (
+    <div>
+      <div className="nextP">
+        <a href="/" className="nextP">
+          <span className="arrowP">&lt;</span> <br />
+          Next 7 Days
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Title;
